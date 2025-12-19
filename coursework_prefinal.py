1c4912e2bfb613487b2bd92b765e701b64a2f5d7
NevereverSo <125371549+NevereverSo@users.noreply.github.com>
2025-12-19 16:06:44 +0500

    Update coursework_prefinal.py

--- coursework_prefinal.py
+++ coursework_prefinal.py
@@ -31,6 +31,8 @@ from sklearn.model_selection import train_test_split
 import warnings
 warnings.filterwarnings('ignore')
 
+pip install statsmodels
+
 # Time series
 from statsmodels.tsa.seasonal import seasonal_decompose
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
